6bb484fb6d9274e038165997148381027b205248
Elodie Morin <Elodie.morin1298@gmail.com>
2019-01-10 17:09:51 +0100

    sticky navbar

---
+++ js/user-menu.js
@@ -0,0 +1,16 @@
+function openUserMenu() {
+    document.getElementsByClassName("user-menu")[0].removeAttribute("hidden");
+}
+
+function closeUserMenu() {
+    document.getElementsByClassName("user-menu")[0].setAttribute("hidden", "true");
+}
+
+function userMenu() {
+    /*
+    if (document.getElementsByClassName("user-menu")[0].getAttribute("hidden")==null) {
+        closeUserMenu
+    }
+    */
+    console.log("coucou");
+}
